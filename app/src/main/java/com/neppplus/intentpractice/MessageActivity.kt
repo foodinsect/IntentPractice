82af1bdd7f42547fd600341711b79167c8f84b2c
Minjin <opqrs0422@naver.com>
2023-06-26 21:20:49 +0900

    전달해준 문구를 메세지 화면에서 사용

--- app/src/main/java/com/neppplus/intentpractice/MessageActivity.kt
+++ app/src/main/java/com/neppplus/intentpractice/MessageActivity.kt
@@ -2,10 +2,19 @@ package com.neppplus.intentpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.neppplus.intentpractice.databinding.ActivityMessageBinding
+import android.content.Intent // 추가
 class MessageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMessageBinding // 추가
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_message)
+        binding = ActivityMessageBinding.inflate(layoutInflater) // 추가
+        setContentView(binding.root)
+
+        // 이 화면에 들어올 때 첨부된 데이터를 텍스트뷰에 반영.
+
+        val intentMessage = intent.getStringExtra("message")
+        binding.receivedMessageTxt.text = intentMessage
+
     }
 }
