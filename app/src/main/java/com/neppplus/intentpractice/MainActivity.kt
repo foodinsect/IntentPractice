3e99e24bf0ed1cea445b22534eecdca2c3c3326d
Minjin <opqrs0422@naver.com>
2023-06-26 20:52:20 +0900

    OtherActivity도 Binding 을 해줘야된다
    Intent 이용 -> 다음 화면으로 넘어가는 기능 구현

--- app/src/main/java/com/neppplus/intentpractice/MainActivity.kt
+++ app/src/main/java/com/neppplus/intentpractice/MainActivity.kt
@@ -3,13 +3,19 @@ package com.neppplus.intentpractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.intentpractice.databinding.ActivityMainBinding // 추가
+import android.content.Intent // 추가
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding // 추가
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater) // 추가
         setContentView(binding.root)
+
+        binding.moveToOtherBtn.setOnClickListener {
+            // 다른 화면으로 이동
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
--- app/src/main/java/com/neppplus/intentpractice/OtherActivity.kt
+++ app/src/main/java/com/neppplus/intentpractice/OtherActivity.kt
@@ -2,10 +2,13 @@ package com.neppplus.intentpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.intentpractice.databinding.ActivityOtherBinding // 수정: ActivityMainBinding -> ActivityOtherBinding
 
 class OtherActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOtherBinding // 수정: ActivityMainBinding -> ActivityOtherBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_other)
+        binding = ActivityOtherBinding.inflate(layoutInflater) // 수정: ActivityMainBinding -> ActivityOtherBinding
+        setContentView(binding.root)
     }
 }
